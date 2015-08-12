3ded7cf92e0b3602f6b3b82b57b50713aa56146b
Tanuj Rastogi <r.tanuj@gmail.com>
2015-08-12 16:32:54 +0530

    controller and maven dependencies pom added

---
+++ src/main/java/org/snap/shopoweb/MainController.java
@@ -0,0 +1,41 @@
+package org.snap.shopoweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    
+    @RequestMapping("/home")
+    public String homePage(ModelMap model){
+        model.addAttribute("message","Hello Spring");
+        return "Hello";
+    }
+    
+    @RequestMapping("/login")
+    public String loginPage(ModelMap model){
+        model.addAttribute("message","Hello Spring");
+        return "Hello";
+    }
+    
+    @RequestMapping("/product")
+    public String productPage(ModelMap model){
+        model.addAttribute("message","Hello Spring");
+        return "Hello";
+    }
+    
+    @RequestMapping("/user")
+    public String userPage(ModelMap model){
+        model.addAttribute("message","Hello Spring");
+        return "Hello";
+    }
+    
+    @RequestMapping("/add")
+    public String addPage(ModelMap model){
+        model.addAttribute("message","Hello Spring");
+        return "Hello";
+    }
+
+}
