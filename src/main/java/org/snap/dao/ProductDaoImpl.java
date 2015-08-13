1b78dd66c2bfe25c26db19cfad219c38b3897811
Suraj Kumar Sau <i@surajsau.in>
2015-08-13 15:05:01 +0530

    select product by loc and pro id

--- src/main/java/org/snap/dao/ProductDaoImpl.java
+++ src/main/java/org/snap/dao/ProductDaoImpl.java
@@ -35,6 +35,20 @@ public class ProductDaoImpl implements ProductDao{
 	
 	}
 	
-	
+	public Product selectProductByLocationAndProductId(int locationId, int productId){
+	    Product product = new Product();
+	    String tableName="product_";
+	    String id=String.valueOf(locationId);
+	    tableName=tableName.concat(id);
+	    
+	    String SQL = "select * from ";
+	    
+	    SQL = SQL.concat(tableName);
+	    SQL = SQL.concat(" where productId = ?");
+	    
+	    product = (Product)jdbcTemplate.queryForObject(SQL, new ProductMapper());
+	    
+	    return product;
+	 }
 
 }
