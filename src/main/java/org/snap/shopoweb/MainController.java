843237c64974b93a77fa32fa77c4cf3bf5d32799
unknown <vinod.bhooker@L-G0Z6152.jasperindia.local>
2015-08-13 15:19:44 +0530

    vinod changes

--- src/main/java/org/snap/shopoweb/MainController.java
+++ src/main/java/org/snap/shopoweb/MainController.java
@@ -1,9 +1,22 @@
 package org.snap.shopoweb;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+
+import org.omg.CORBA.Request;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -32,10 +45,20 @@ public class MainController {
         return "Hello";
     }
     
-    @RequestMapping("/add")
-    public String addPage(ModelMap model){
-        model.addAttribute("message","Hello Spring");
-        return "Hello";
-    }
+    //@RequestMapping("/add.htm")
+   //public ModelAndView addPage(@ModelAttribute("user") User user) throws ServletException, IOException{
+    	/*ModelAndView model = new ModelAndView("add");
+    	ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+    	//UserDapImpl u= UserDapImpl(ac.getBean("userDao"));
+    	user.setUserName(user.getUserName());
+    	user.setUserPassword(user.getUserPassword());
+    	user.setUserEmail(user.getUserEmail());
+    	user.setUserContact(user.getUserContact());
+    	
+    	
+    	u.insertUser(user);
+    	
+        return model;*/
+    //}
 
 }
