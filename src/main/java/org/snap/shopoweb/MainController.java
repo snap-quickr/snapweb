0242d62257a06e57cdcc0999c309979282f42dcf
Suraj Kumar Sau <i@surajsau.in>
2015-08-13 15:02:01 +0530

    ...

--- src/main/java/org/snap/shopoweb/MainController.java
+++ src/main/java/org/snap/shopoweb/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -22,9 +23,8 @@ public class MainController {
     }
     
     @RequestMapping("/product")
-    public String productPage(ModelMap model){
-       // model.addAttribute("toDo",DataContainer.getHm().get("search"));
-        return "Hello";
+    public ModelAndView productPage(ModelMap model){
+       return new ModelAndView("details");
     }
     
     @RequestMapping("/user")
